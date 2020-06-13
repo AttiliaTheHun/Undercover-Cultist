68d2da29d1843bd3c87e8bb645dcbd05a687f937
Glitch (starter-discord) <none>
2020-06-13 09:16:18 +0000

    📻🍥 Checkpoint ./commands/server.js:10858270/52

--- commands/server.js
+++ commands/server.js
@@ -14,11 +14,11 @@ execute(message, args, client){
   .setThumbnail(message.guild.iconURL()) 
   .addField('Owner', message.guild.owner.user.tag) 	
   .addField('Region', message.guild.region)
-  .addField('Created', message.guild.createdAt)	
+  .addField('Created', message.guild.createdAt.substring(0, 31))	
   .addField('Members', message.guild.memberCount, true) 	
   .addField('Bots', message.guild.members.cache.filter(member => member.user.bot).size, true)
   .addField('Roles', message.guild.roles.cache.size, true)
-  .addField('Admins', message.guild.members.cache.filter(member => member
+  .addField('Admins', message.guild.members.cache.filter(member => member.hasPermission('ADMINISTRATOR')).size)
   .addField('Channel Categories', message.guild.channels.cache.filter(channel => channel.type === "category").size)
   .addField('Text Channels', message.guild.channels.cache.filter(channel => channel.type === "text").size)
   .addField('Voice Channels',message.guild.channels.cache.filter(channel => channel.type === "voice").size)
