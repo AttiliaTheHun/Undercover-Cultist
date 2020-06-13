8fc87dad67ccbbf6857d5b7924ce9b078a62676b
Glitch (starter-discord) <none>
2020-06-13 07:54:21 +0000

    📈⛑ Checkpoint ./commands/server.js:10858270/350

--- commands/server.js
+++ commands/server.js
@@ -3,27 +3,21 @@ module.exports = {
 name: 'server',
 description: 'shows server info',
 execute(message, args, client){
-  
- /* let count = 0;
-  message.guild.fetchMembers().then(g => {
-       count = g.members.length; 
-       g.members.forEach((member) => {
-            count++;
-        });
-    }); */
+
 
   
   const embed = new Discord.MessageEmbed()
-.setColor(' #366A1B') 	
+.setColor('#366A1B') 	
   .setTitle(`${message.guild.name}`) 	
-  .setAuthor('Here is the latest report', '') 	
-  .setDescription('') 
+  .setAuthor('', '') 	
+  .setDescription('Here is the last report') 
   .setThumbnail(message.guild.iconURL()) 
-  .addField('Owner', message.guild.owner, true) 	
+  .addField('Owner', message.guild.owner.user.tag, true) 	
   .addField('Created', message.guild.createdAt, true) 	
   .addField('Members', message.guild.memberCount, true) 	
   .addField('Bots', message.guild.members.cache.filter(member => member.user.bot).size, true) 
-  .addField('Roles',
+  .addField('Roles', message.guild.roles.cache.size, true)
+  .addField('Channels', message.guild.channels.cache.size, true)
   .setTimestamp() 	
   .setFooter('We could benefit from having someone on the inside', client.user.avatarURL());
   message.channel.send(embed);
