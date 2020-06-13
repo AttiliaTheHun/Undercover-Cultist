e376ffed5322a08a2e196fc1bc240e81ef0b0814
Glitch (starter-discord) <none>
2020-06-13 07:42:15 +0000

    🚄🏋 Checkpoint ./commands/server.js:10858270/242

--- commands/server.js
+++ commands/server.js
@@ -4,27 +4,28 @@ name: 'server',
 description: 'shows server info',
 execute(message, args, client){
   
-  let count = 0;
+ /* let count = 0;
   message.guild.fetchMembers().then(g => {
        count = g.members.length; 
-       /* g.members.forEach((member) => {
+       g.members.forEach((member) => {
             count++;
-        });*/
-    });
+        });
+    }); */
 
   
   const embed = new Discord.MessageEmbed()
 .setColor(' #366A1B') 	
   .setTitle(`${message.guild.name}`) 	
-  .setAuthor('Here is the latest report', 'https://discord.js.org') 	
-  .setDescription('Some description here') 
+  .setAuthor('Here is the latest report', '') 	
+  .setDescription('') 
   .setThumbnail(message.guild.iconURL()) 
   .addField('Owner', message.guild.owner, true) 	
   .addField('Created', message.guild.createdAt, true) 	
-  .addField('Members', count, true) 	
-  .addField('Inline field title', 'Some value here', true) 
+  .addField('Members', message.guild.memberCount, true) 	
+  .addField('Bots', message.guild.members.cache.filter(member => member.user.bot).size, true) 
+  .addField('Roles',
   .setTimestamp() 	
-  .setFooter('We could benefit from having someone on the inside', client.avatarURL());
+  .setFooter('We could benefit from having someone on the inside', client.user.avatarURL());
   message.channel.send(embed);
   
 },
