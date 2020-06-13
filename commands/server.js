430774f906fee9b626fef09a95d534948b187627
Glitch (starter-discord) <none>
2020-06-13 08:53:12 +0000

    🚎✍️ Checkpoint ./commands/server.js:10858270/179

--- commands/server.js
+++ commands/server.js
@@ -8,20 +8,20 @@ execute(message, args, client){
   
   const embed = new Discord.MessageEmbed()
 .setColor('#005E1F')
-  .setTitle(`${message.guild.name}     ID:${message.guild.id}`) 	
+  .setTitle(`${message.guild.name}`) 	
   .setAuthor('', '') 	
   .setDescription('Here is the latest report') 
   .setThumbnail(message.guild.iconURL()) 
   .addField('Owner', message.guild.owner.user.tag) 	
   .addField('Region', message.guild.region)
   .addField('Created', message.guild.createdAt)	
-  .addField('Members', message.guild.memberCount) 	
-  .addField('Bots', message.guild.members.cache.filter(member => member.user.bot).size)
-  .addField('Roles', message.guild.roles.cache.size)
-  .addField('Channel Categories', message.guild.categories.cache.size)
-  .addField('Text Channels', message.guild.channels.cache.filter(channel => channel.type.text).size)
+  .addField('Members', message.guild.memberCount, true) 	
+  .addField('Bots', message.guild.members.cache.filter(member => member.user.bot).size, true)
+  .addField('Roles', message.guild.roles.cache.size, true)
+  .addField('Channel Categories', message.guild.channels.cache.filter(channel => channel.type.category).size)
+  .addField('Text Channels', message.guild.channels.cache.filter(channel => channel.type == 0).size)
   .addField('Voice Channels',message.guild.channels.cache.filter(channel => channel.type.voice).size)
-  
+  .addField('ID', message.guild.id)
   .setTimestamp() 	
   .setFooter('We could benefit from having someone on the inside', client.user.avatarURL());
   message.channel.send(embed);
